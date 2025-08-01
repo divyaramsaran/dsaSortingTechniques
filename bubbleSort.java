e2ad3241e3766335e5a245d2242ea7d9bde717a2
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-01 17:00:51 +0000

    takes dynamic input

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
 
+class sort {
+    void sortArray () {
+
+    }
+}
+
 class bubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array");
         int length = sc.nextInt();
         System.out.println(length);
+        int array[] = new int[length];
+        System.out.println("Enter array elements");
+        for(int range = 0; range < length; range++) {
+                array[range] = sc.nextInt();
+        }
     }
 }
