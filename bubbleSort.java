c36197f2c3c3139fc2799517378afd19d871bcb6
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-01 16:39:57 +0000

    takes length of the array

---
+++ bubbleSort.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class bubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int length = sc.nextInt();
+        System.out.println(length);
+    }
+}
