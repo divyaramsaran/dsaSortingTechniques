8c23efbe7e60709eb7c39c9f23bdef0def3cc8d4
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-02 17:04:56 +0000

    refactors loop and lint issues

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 class sort {
-    int sortArray (array, length) {
-        for(int index = 0; index < length; index++) {
-            for(int subIndex = 0; subIndex < length - i; subIndex++) {
-                if(array[subIndex] > arr[subIndex + 1]) {
+    int[] sortArray (int [] array, int length) {
+        for(int index = 0; index < length - 1; index++) {
+            for(int subIndex = 0; subIndex < length - 1 - index; subIndex++) {
+                if(array[subIndex] > array[subIndex + 1]) {
                     array[subIndex] = array[subIndex] + array[subIndex + 1];
                     array[subIndex + 1] = array[subIndex] - array[subIndex + 1];
                     array[subIndex] = array[subIndex] - array[subIndex + 1];
@@ -21,11 +21,14 @@ class bubbleSort {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array");
         int length = sc.nextInt();
-        System.out.println(length);
         int array[] = new int[length];
         System.out.println("Enter array elements");
+
         for(int range = 0; range < length; range++) {
                 array[range] = sc.nextInt();
         }
+
+        sort obj = new sort();
+        int [] sortedArray = obj.sortArray(array, length);
     }
 }
