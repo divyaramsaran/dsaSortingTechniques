0f86dcbf67a19cbe344c75f410ee0aefe5219e11
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-05 16:52:09 +0000

    takes the array length

---
+++ mergeSort.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class mergeSort {
+    public static void main(String [] args){
+        System.out.println("Enter the array length");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+    }
+}
