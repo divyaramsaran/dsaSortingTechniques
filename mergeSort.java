f7ede07788ff11e3423b0641c80476827e661abb
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-05 16:57:16 +0000

    takes array elements

--- mergeSort.java
+++ mergeSort.java
@@ -5,5 +5,14 @@ class mergeSort {
         System.out.println("Enter the array length");
         Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
+        int array [] = new int [length];
+        System.out.println("Enter the array elements");
+
+        for(int range = 0; range < length; range++) {
+            array[range] = sc.nextInt();
+        }
+        for(int index = 0; index < length; index++) {
+            System.out.println(array[index]);
+        }
     }
 }
