ccbfe87a597a3d97d78d02e6da8221877fb8c698
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-08 16:09:16 +0000

    adds sort class

--- mergeSort.java
+++ mergeSort.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 
+class sort {
+    void sortArray(int [] array, int start, int end) {
+        if(start < end) {
+            int mid = (start + end) / 2;
+            sortArray(array, start, mid);
+            sortArray(array, mid + 1, end);
+            merge(array, start, mid, end);
+        }
+    }
+}
+
 class mergeSort {
     public static void main(String [] args){
         System.out.println("Enter the array length");
@@ -11,6 +22,10 @@ class mergeSort {
         for(int range = 0; range < length; range++) {
             array[range] = sc.nextInt();
         }
+
+        sort obj = new sort();
+        obj.sortArray(array, 0, length - 1);
+
         for(int index = 0; index < length; index++) {
             System.out.println(array[index]);
         }
