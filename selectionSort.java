0bc7e61ad83e621a6a8d50d9bbd2d4c812efe0a3
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-13 15:39:59 +0000

    adds sort class and sorts the array

--- selectionSort.java
+++ selectionSort.java
@@ -1,5 +1,24 @@
 import java.util.Scanner;
 
+class sort {
+    int [] sortArray (int [] array, int length) {
+        for(int index = 0; index < length - 1; index++) {
+            int minIndex = index;
+            for(int subIndex = index + 1; subIndex < length ; subIndex++) {
+                    if(array[subIndex] < array[minIndex]) {
+                        minIndex = subIndex;
+                    }
+            }
+            if(index != minIndex) {
+                array[index] = array[index] + array[minIndex];
+                array[minIndex] = array[index] - array[minIndex];
+                array[index] = array[index] - array[minIndex];
+            }
+        }
+        return array;
+    }
+}
+
 public class selectionSort {
     public static void main(String args []) {
         System.out.println("Enter the array length");
@@ -7,8 +26,15 @@ public class selectionSort {
         int length = sc.nextInt();
         System.out.println("Enter array elements");
         int array [] = new int [length];
+
         for (int index = 0; index < length; index++) {
             array[index] = sc.nextInt();
+        }
+
+        sort obj = new sort();
+        int [] sortedArray = obj.sortArray(array, length);
+        for(int element : sortedArray) {
+            System.out.println(element);
         }
     }
 }
